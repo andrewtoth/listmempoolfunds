684df84a26abdb5b084f2e1bf6af10cd7f1ce491
Andrew Toth <andrewstoth@gmail.com>
2021-12-15 21:19:25 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# listmempoolfunds
+
+This plugin adds the ability to track unconfirmed wallet deposits. It introduces
+a new method `listmempoolfunds` that has the same signature and response format
+as `listfunds`, except it will also return unconfirmed received outputs.
+
+## How does listmempoolfunds work
+
+It leverages Bitcoin Core's watchonly descriptor wallet and clightning's
+hsmtool. By creating a new descriptor wallet and importing the node's xpubs,
+Bitcoin Core will track all incoming payments. Since bitcoind handles tracking
+the payments in the mempool, the plugin can query bitcoind for any new payments
+that aren't present in `listfunds`. These new payments can be appended to the
+`listfunds` response and returned, and it appears just like `listfunds` is
+tracking unconfirmed deposits!
+
+## Dependencies
+
+This plugin depends on having RPC access to a running Bitcoin Core. It also
+needs the wallet compiled in and not disabled (`disablewallet=0`), and it needs
+to not be running in blocks-only mode (`blocksonly=0`).
+It *can* be running in pruned mode (`prune=<n>`), but you will need to have
+blocks back to at least where the first deposit to the node was made. After it
+has imported the descriptors and rescanned, older blocks can be pruned.
+
+## Installation
+
+Run `pip3 install --user -r -requirements.txt` and include the
+`listmempoolfunds.py` file as a plugin via instructions [here](https://github.com/lightningd/plugins/#installation).
+
+Set the `listmempoolfunds-rpcuser` and `listmempoolfunds-rpcpassword` options to
+the credentials of your bitcoind instance.
+You must also set the `listmempoolfunds-descriptor` option twice in order to
+import the proper descriptors. If you are using the default locations, this can
+be done with the following command:
+```
+$ ./tools/hsmtool dumponchaindescriptors ~/.lightning/bitcoin/hsm_secret | xargs -n1 printf "listmempoolfunds-descriptor=%s\n" >> ~/.lightning/config
+```
+
+On startup, the plugin will check if the wallet exists and if it is loaded. If
+not, it will create the wallet, import the descriptors, and perform a rescan.
+On mainnet the rescan can take several hours, but it only has to happen once.
---
+++ listmempoolfunds.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python3
+'''List received funds that are still in the mempool
+'''
+from pyln.client import Plugin
+from decimal import Decimal
+from socket import timeout
+
+from bitcoin.rpc import Proxy, JSONRPCError, InvalidAddressOrKeyError
+
+plugin = Plugin(dynamic=False)
+
+@plugin.init()
+def init(options: dict, configuration: dict, plugin: Plugin, **kwargs):
+    info = plugin.rpc.getinfo()
+    # Create a desriptor wallet named clightning-<alias>-<id> so it will be unique
+    wallet_name = f'clightning-{info["alias"]}-{info["id"]}'
+    host = options['listmempoolfunds-rpchost']
+    user = options['listmempoolfunds-rpcuser']
+    password = options['listmempoolfunds-rpcpassword']
+    port = options.get('listmempoolfunds-rpcport')
+    if port == 'null':
+        network = configuration['network']
+        if network == 'testnet':
+            port = '18332'
+        elif network == 'regtest':
+            port = '18443'
+        elif network == 'signet':
+            port = '38332'
+        else:
+            port = '8332'
+
+    plugin.service_url = f'http://{user}:{password}@{host}:{port}/wallet/{wallet_name}'
+    proxy = Proxy(service_url=plugin.service_url)
+
+    wallets: list
+    try:
+        wallets = proxy.call('listwallets')        
+    except JSONRPCError as e:
+        if e.args[0]['message'] == 'Method not found':
+            plugin.log('Could not find listwallets method. Is the wallet enabled?', 'error')
+            return { 'disable': 'Could not find listwallets method. Is the wallet enabled?' }
+        plugin.log(e.args[0]['message'], 'error')
+        return { 'disable': e.args[0]['message'] }
+    except Exception as e:
+        plugin.log(f'Could not connect to {service_url}\nPlease check all listmempoolfunds options.', 'error')
+        return { 'disable': f'Could not connect to {service_url}\nPlease check all listmempoolfunds options.' }
+
+    if wallet_name in wallets:
+        plugin.log(f'Wallet {wallet_name} already loaded.')
+        return {}
+    else:
+        try:
+            proxy.call('loadwallet', wallet_name)
+            plugin.log(f'Loaded wallet {wallet_name}.')
+            return {}
+        except JSONRPCError as e:
+            if e.args[0]['code'] != -18: # Code -18 is returned if the wallet doesn't exist
+                plugin.log(e.args[0]['message'], 'error')
+                return { 'disable': e.args[0]['message'] }
+
+    # Determine if we have the right descriptors
+    raw_descs = options['listmempoolfunds-descriptor']
+    num_descs = 2
+    if len(raw_descs) != num_descs:
+        plugin.log('Both descriptors were not included as options.', 'error')
+        return { 'disable': 'Both descriptors were not included as options.' }
+
+    # Check the descriptors so we don't create the wallet and then can't import
+    descs = []
+    for desc in raw_descs:
+        try:
+            proxy.call('getdescriptorinfo', desc)
+        except InvalidAddressOrKeyError as e:
+            plugin.log('Invalid descriptors. Check the network argument when dumping from hsmtool.', 'error')
+            return { 'disable': 'Invalid descriptors. Check the network argument when dumping from hsmtool.' }
+        descs.append({ 'desc': desc, 'timestamp': 'now' })
+    
+    # Create the wallet and import the descriptors
+    try:
+        proxy.call('createwallet', wallet_name, True, False, '', False, True, True, False)
+        plugin.log(f'Created wallet {wallet_name}')
+
+        resps = proxy.call('importdescriptors', descs)
+        for i in range(num_descs):
+            if resps[i]['success']:
+                plugin.log(f'Imported descriptor {raw_descs[i]}.')
+            else:
+                plugin.log(f'Failed to import descriptor {raw_descs[i]}: {resps[i]["error"]["message"]}')
+        
+        plugin.log('Rescanning blockchain. Check bitcoind logs for progress.')
+        proxy.call('rescanblockchain', 481824) # Rescan at segwit block height
+    except JSONRPCError as e:
+        plugin.log(e.args[0]['message'], 'error')
+    except timeout:
+        # rescanblockchain only returns after it's finished, so just catch the socket timeout instead of waiting
+        pass
+
+    return {}
+
+@plugin.method('listmempoolfunds')
+def list_mempool_funds(plugin: Plugin, spent=False):
+    """Show available funds from the internal wallet.
+    Identical to listfunds, but also includes received funds that are still in the mempool.
+    """
+    listfunds_resp = plugin.rpc.listfunds(spent)
+    txids = list(map(lambda unspent: unspent['txid'], listfunds_resp['outputs']))
+    unspent_list: list
+    try:
+        proxy = Proxy(service_url=plugin.service_url)
+        unspent_list = proxy.call('listunspent', 0, 100)
+    except Exception as e:
+        plugin.log(f'Exception {str(e)}', 'error')
+        return { 'code': -1, 'message': 'Could not connect to bitcoind.' }
+
+    new_unspent = list(filter(lambda unspent: unspent['txid'] not in txids, unspent_list))
+    for unspent in new_unspent:
+        amount = round(unspent['amount'] * Decimal(1e8))
+        output = {
+            'txid': unspent['txid'],
+            'output': unspent['vout'],
+            'value': amount,
+            'amount_msat': str(amount) + '000msat',
+            'scriptpubkey': unspent['scriptPubKey'],
+            'address': unspent['address'],
+            'status': 'unconfirmed',
+            'reserved': 'false'
+        }
+        listfunds_resp['outputs'].insert(0, output)
+    return listfunds_resp
+
+plugin.add_option(
+    'listmempoolfunds-rpchost',
+    '127.0.0.1',
+    'bitcoind RPC host',
+    'string'
+)
+plugin.add_option(
+    'listmempoolfunds-rpcport',
+    None,
+    'bitcoind RPC port (inferred by network if not supplied)',
+    'string'
+)
+plugin.add_option(
+    'listmempoolfunds-rpcuser',
+    'user',
+    'bitcoind RPC user',
+    'string'
+)
+plugin.add_option(
+    'listmempoolfunds-rpcpassword',
+    'passwd',
+    'bitcoind RPC password',
+    'string'
+)
+plugin.add_option(
+    'listmempoolfunds-descriptor',
+    None,
+    'Wallet descriptor used for receiving and change. '
+    'Retrieved using dumponchaindescriptors via the hsmtool.'
+    'Must have both descriptors included as separate options.',
+    'string',
+    False,
+    True
+)
+
+plugin.run()
